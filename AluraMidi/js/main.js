015976a3343ddc7519ca47390bc73a674eac6e60
Guilherme Augusto de Oliveira Carvalho <77821746+guiaocarvalhoo@users.noreply.github.com>
2022-08-23 17:35:14 -0300

    Adicionando arquivos do projeto
    Projeto do AluraMidi concluído

---
+++ AluraMidi/js/main.js
@@ -0,0 +1,46 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+        
+    } 
+    
+    else {
+        
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+   
+}
+const listaDeTeclas = document.querySelectorAll ('.tecla');
+
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+    
+    const tecla = listaDeTeclas[i]
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento} ` //Template String
+    
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (event) {
+        
+        console.log(event.code);
+
+        if (event.code === "Enter" || event.code === "Space") {
+            tecla.classList.add('ativa');
+        } 
+            
+            tecla.onkeyup = function () {
+            tecla.classList.remove('ativa');
+            
+        }
+
+    }
+
+
+    
+}
